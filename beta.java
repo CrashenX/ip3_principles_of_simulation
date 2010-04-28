58e6053c40e918c258646e76ebe0a9efc94f428b
Jesse Cook <jesse@Jesse-Cooks-MacBook.local>
2010-04-28 18:05:43 -0500

    added initial code for hung's distros

--- beta.java
+++ beta.java
@@ -1,5 +1,5 @@
 class Distribution {
-    // percent down parameters
+    // percent down parameters - beta
     double perc_down_https_y_max        = 2.33;
     double perc_down_http_0_75_y_max    = 5.53;
     double perc_down_http_75_100_y_max  = 5.36;
@@ -23,13 +23,29 @@ class Distribution {
     double perc_down_http_75_100_x_min  = 0.75000;
     double perc_down_http_75_100_x_max  = 0.99999;
 
-    // total byte parameters (https)
+    // total byte parameters - gamma (https)
     double tot_bytes_https_y_max        = 0.00012;
     double tot_bytes_https_shape        = 3.4688;
     double tot_bytes_https_scale        = 2133;
     double tot_bytes_https_gamma_shape  = 3.21143;
     double tot_bytes_https_x_min        = 0;
     double tot_bytes_https_x_max        = 20000;
+
+    // bps down parameters - beta
+    double perc_down_https_y_max        = 0.00000518637;
+    // b == gamma(beta1 + beta2)/(gamma(beta1)gamma(beta2))
+    double bps_down_https_b             = 6.27668;
+    double bps_down_https_beta_1        = 1.1806;
+    double bps_down_https_beta_2        = 4.3438;
+    double bps_down_https_x_min         = 6476.9;
+    double bps_down_https_x_max         = 600000;
+    // bps down parameters - gamma
+    double bps_down_http_y_max          = 0.00000699396;
+    double bps_down_http_shape          = 1.6837;
+    double bps_down_http_scale          = 61445;
+    double bps_down_http_gamma_shape    = 0.905651;
+    double bps_down_http_x_min          = 0;
+    double bps_down_http_x_max          = 600000;
 
     double beta_fx(double x, double b, double beta_1, double beta_2,
                    double x_min, double x_max) {
@@ -128,10 +144,39 @@ class Distribution {
     }
 
     double calc_bps_down_https() {
-        return 0;
+        double x    = 0;
+        double f_x  = 0;
+        double y    = 0;
+
+        do {
+            double rnd1 = generator.nextDouble();
+            if(rnd1 == 0 || rnd1 == 1) continue;
+            double rnd2 = generator.nextDouble();
+            x = calc_x(bps_down_https_x_min, bps_down_https_x_max, rnd1);
+            y = calc_y(bps_down_https_y_max, rnd2);
+            f_x = beta_fx(x, bps_down_https_b,
+                          bps_down_https_beta_1, bps_down_https_beta_2,
+                          bps_down_https_x_min,  bps_down_https_x_max);
+        }while(y > f_x);
+
+        return x;
     }
 
     double calc_bps_down_http() {
-        return 0;
+        double x    = 0;
+        double f_x  = 0;
+        double y    = 0;
+
+        do {
+            double rnd1 = generator.nextDouble();
+            if(rnd1 == 0 || rnd1 == 1) continue;
+            double rnd2 = generator.nextDouble();
+            x = calc_x(bps_down_http_x_min, bps_down_http_x_max, rnd1);
+            y = calc_y(bps_down_http_y_max, rnd2);
+            f_x = gamma_fx(x, bps_down_http_gamma_shape,
+                          bps_down_http_shape, bps_down_http_scale);
+        }while(y > f_x);
+
+        return x;
     }
 }
